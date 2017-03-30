3cb34209a4639653647774e85daf53b489ba12f8
Ben Dalton <bendalton@Dalton-MBP.local>
2017-03-29 23:01:18 -0400

    toggles classes for animations

--- src/components/level.js
+++ src/components/level.js
@@ -4,40 +4,6 @@ import { LevelEnter, LevelLeave } from './styles/transform-styles';
 
 class Level extends Component {
   render() {
-    const Div = styled.div`
-      position: relative;
-      width: 100%;
-      height: 100%;
-      cursor: pointer;
-      pointer-events: auto;
-      -webkit-transition: opacity 1s, -webkit-transform 1s;
-      transition: opacity 1s, transform 1s;
-      -webkit-transition-timing-function: cubic-bezier(0.7, 0, 0.3, 1);
-      transition-timing-function: cubic-bezier(0.7, 0, 0.3, 1);
-      -webkit-transform-style: preserve-3d;
-      transform-style: preserve-3d;
-      transform: translateZ(10vmin);
-      -ms-transform: translateZ(10vmin);
-      -webkit-transform: translateZ(0vmin);
-      &:not(:first-child) {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-      }
-      &.level--2 {
-        -webkit-transform: translateZ(10vmin);
-        transform: translateZ(10vmin);
-      }
-      &.level--3 { -webkit-transform: translateZ(20vmin);
-        transform: translateZ(20vmin);
-      }
-
-      &.level--4 {
-        -webkit-transform: translateZ(30vmin);
-        transform: translateZ(30vmin);
-      }
-    `;
-
     const Svg = styled.svg`
       width: 90vw;
       height: 90vh;
@@ -54,15 +20,16 @@ class Level extends Component {
         cursor: pointer;
     `;
     return (
-      <Div
-        className={`level level--${this.props.level.id}`}
+      <div
+        className={`level level--${this.props.level.id}
+          ${this.props.level.id === this.props.selectedLevelId ? 'level--current' : ''}`}
         style={this.props.selectedZoneId ? LevelLeave : LevelEnter}
         onClick={() => this.props.onSelectLevel(this.props.level.id)}
       >
         <Svg version="1.1" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 200 500" x="0px" y="0px" xmlSpace="preserve">
           <Polygon class="st0" fill={this.props.level.color} points={this.props.level.points} />
         </Svg>
-      </Div>
+      </div>
     );
   }
 };
--- src/components/map-component.js
+++ src/components/map-component.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import styled from 'styled-components';
 import Footprint from './footprint';
 import Level from './level';
 import './styles/transitions.css';
@@ -11,44 +9,27 @@ class MapComponent extends Component {
     document.body.className = 'animated fadeIn';
   }
   render() {
-    const Levels = styled(ReactCSSTransitionGroup)`
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      width: 96vmin;
-      height: 64vmin;
-      margin: -32vmin 0 0 -48vmin;
-      -webkit-transition: -webkit-transform 0.3s;
-      transition: transform 0.3s;
-      -webkit-transform-style: preserve-3d;
-      transform-style: preserve-3d;
-      -webkit-transform: rotateX(70deg) rotateZ(-45deg) translateZ(-15vmin);
-      transform: rotateX(70deg) rotateZ(-45deg) translateZ(-15vmin);
-    `;
     return (
       <div className="mall">
         <Footprint
           selectedZoneId={this.props.selectedZoneId}
           onSelectZone={this.props.onSelectZone}
         />
-        <Levels
-          transitionName='animate-levels'
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={500}
-          transitionAppear={true}
-          transitionAppearTimeout={500}
-          component='div'
-          className='levels'>
-          {this.props.selectedZoneId && this.props.activeLevels.map(l => {
-            return <Level
-                      selectedZoneId={this.props.selectedZoneId}
-                      onSelectLevel={this.props.onSelectLevel}
-                      key={l.id || 1010101}
-                      level={l}
-                    />
-            }
-          )}
-        </Levels>
+        { this.props.selectedZoneId &&
+          <div
+            className={this.props.selectedLevelId ? `levels levels--open levels--selected-${this.props.selectedLevelId}` : 'levels'}>
+            {this.props.selectedZoneId && this.props.activeLevels.map(l => {
+              return <Level
+                        selectedZoneId={this.props.selectedZoneId}
+                        onSelectLevel={this.props.onSelectLevel}
+                        selectedLevelId={this.props.selectedLevelId}
+                        key={l.id || 1010101}
+                        level={l}
+                      />
+              }
+            )}
+          </div>
+        }
       </div>
     );
   }
